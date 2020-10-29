5eef96ad10883ed5842efaa116acf37c22a93428
bryan oregel <bryan.martinez1191@alumnos.udg.mx>
2020-10-28 22:36:47 -0600

    archivo completo

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "arreglo.h"
+
+using namespace std;
+void modificar(int *a)
+{
+    *a = *a +1;
+
+}
+
+int main()
+{
+    Arreglo arreglo;
+    arreglo.insertar_final(10);
+    arreglo.insertar_final(0);
+    arreglo.insertar_final(1);
+    arreglo.insertar_final(4);
+    arreglo.insertar_final(-10);
+
+    for (size_t i = 0; i < arreglo.size(); i++)
+    {
+        cout << arreglo[i] << " ";
+    }
+
+
+
+    return 0;
+}
